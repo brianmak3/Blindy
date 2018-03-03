93ba6caf35f158c2cd5e371eb50e78fde7258096
brianhenry <brianworkss@gmail.com>
2018-03-03 19:02:44 +0300

    Added files and changes required for deployment to heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
  //   'url' :'mongodb://brianhenry:Maryhenry@ds153501.mlab.com:53501/noselv'
-    'url' :'mongodb://27017/toyebana'
+    'url' :'mongodb://root:JIzmXTIXVw38@127.0.0.1/toyebana'
 };
