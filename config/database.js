4c4230ceef7215c632b17a2a4a1e79057bd77469
brianhenry <brianworkss@gmail.com>
2018-03-13 21:20:16 +0300

    commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
  //   'url' :'mongodb://brianhenry:Maryhenry@ds153501.mlab.com:53501/noselv'
 
-    'url' :'mongodb://admin:password@127.0.0.1/toyebana'
+    'url' :'mongodb://root:JIzmXTIXVw38@127.0.0.1/toyebana'
 };
